5b42834490b99583a15dedfd1cc1acfafbda73e0
Azajul-Karim <azajulkarim2021@gmail.com>
2023-04-20 15:26:25 +0600

    first commit

---
+++ 01-variables-data-types/01.console/script.js
@@ -0,0 +1,24 @@
+console.log(100);
+
+console.log("Hello");
+
+console.log(20, "hello", true);
+
+const x = 100;
+
+console.log(x);
+
+console.error("Alert");
+
+console.warn("Warning");
+
+console.table({ name: "topu", age: 23 });
+
+console.group("simple");
+console.log(x);
+console.error("Alert");
+console.warn("Warning");
+console.groupEnd();
+
+const styles = "padding: 10px;, background-color: white; color: green;";
+console.log("%cHello World", styles);
